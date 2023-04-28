cb9b44cd506d377437f9c97a4f7a97033c65df11
Kais <baccour.kais@yahoo.fr>
2023-04-28 20:08:11 -0300

    compromise-dev-ops

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM denoland/deno:alpine
+
+ARG CONFIG_FILE
 COPY . /opt/pingpong
 WORKDIR /opt/pingpong
+COPY $CONFIG_FILE /opt/pingpong/env.ts
+
 CMD deno run --allow-read --allow-net --allow-env main.ts --mode=loop
---
+++ config.ts
@@ -0,0 +1,17 @@
+import { GasPrice } from "npm:@cosmjs/stargate";
+
+export interface Config {
+  // dapp chain
+  endpoint: string;
+  monitoringContract: string;
+  proxyContract: string;
+  mnemonic: string;
+  feeDenom: string;
+  addressPrefix: string;
+  chainId: string;
+  gasPrice: GasPrice;
+
+  // nois chain (Testnet 005)
+  noisEndpoint: string;
+  drandContract: string;
+}
