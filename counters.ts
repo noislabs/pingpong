049ffd7d153a950ac76af4ce2a38aa80503220b5
Simon Warta <simon@warta.it>
2023-06-24 12:49:12 +0200

    Add success/error/timeout counters

---
+++ counters.ts
@@ -0,0 +1,23 @@
+import { promclient } from "./deps.ts";
+
+/** Global result counters. For each run, one of the counters is incremented. */
+export function makePingpongCounters() {
+  return {
+    success: new promclient.Counter({
+      name: "pingpong_success",
+      help: "Number of successful pingpong runs.",
+      labelNames: ["chainId"] as const,
+    }),
+    timeout: new promclient.Counter({
+      name: "pingpong_timeout",
+      help: "Number pingpong runs that times out.",
+      labelNames: ["chainId"] as const,
+    }),
+    error: new promclient.Counter({
+      name: "pingpong_error",
+      help:
+        "Number of failed pingpong runs. This is incremented in case of an exception. Timeouts are not in here.",
+      labelNames: ["chainId"] as const,
+    }),
+  };
+}
--- deps.ts
+++ deps.ts
@@ -4,3 +4,5 @@ export {} from "npm:@cosmjs/utils@^0.31.0";
 export { sha256 } from "npm:@cosmjs/crypto@^0.31.0";
 export { toHex } from "npm:@cosmjs/encoding@^0.31.0";
 export { Tendermint34Client } from "npm:@cosmjs/tendermint-rpc@^0.31.0";
+
+export * as promclient from "npm:prom-client";
