19fc1096db0bc8f09e92949543640eba4bf2e6bd
Simon Warta <simon@warta.it>
2023-10-07 17:54:59 +0300

    Let roundAfter work on nanosecond precision

--- drand.ts
+++ drand.ts
@@ -1,19 +1,22 @@
-const DRAND_GENESIS = new Date(1677685200_000);
-const DRAND_ROUND_LENGTH = 3_000; // in milliseconds
+const DRAND_GENESIS_S = 1677685200;
+const DRAND_GENESIS_NS = BigInt(DRAND_GENESIS_S) * BigInt(1_000_000_000);
+const DRAND_GENESIS = new Date(DRAND_GENESIS_S * 1000);
+const DRAND_ROUND_LENGTH_MS = 3_000; // in milliseconds
+const DRAND_ROUND_LENGTH_NS = BigInt(3_000_000_000); // in nanoseconds
 
 // See TimeOfRound implementation: https://github.com/drand/drand/blob/eb36ba81e3f28c966f95bcd602f60e7ff8ef4c35/chain/time.go#L30-L33
 export function timeOfRound(round: number): Date {
-  return new Date(DRAND_GENESIS.getTime() + (round - 1) * DRAND_ROUND_LENGTH);
+  return new Date(DRAND_GENESIS.getTime() + (round - 1) * DRAND_ROUND_LENGTH_MS);
 }
 
-function roundAfter(base: Date): number {
+function roundAfterImpl(base: bigint): number {
   // Losely ported from https://github.com/drand/drand/blob/eb36ba81e3f28c966f95bcd602f60e7ff8ef4c35/chain/time.go#L49-L63
-  if (base < DRAND_GENESIS) {
+  if (base < DRAND_GENESIS_NS) {
     return 1;
   } else {
-    const from_genesis = base.getTime() - DRAND_GENESIS.getTime();
-    const periods_since_genesis = Math.floor(from_genesis / DRAND_ROUND_LENGTH);
-    const next_period_index = periods_since_genesis + 1;
+    const from_genesis = base - DRAND_GENESIS_NS;
+    const periods_since_genesis = from_genesis / DRAND_ROUND_LENGTH_NS;
+    const next_period_index = Number(periods_since_genesis + BigInt(1));
     return next_period_index + 1; // Convert 0-based counting to 1-based counting
   }
 }
@@ -24,6 +27,6 @@ function roundAfter(base: Date): number {
  * but could just read it from the content of the IBC acknowledegement but
  * for now this is easier.
  */
-export function validRoundAfter(after: Date): number {
-  return roundAfter(after);
+export function roundAfter(after: string | bigint): number {
+  return roundAfterImpl(BigInt(after));
 }
