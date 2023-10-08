9cb0f7d8633099bc5aca4bb5a992498cd3053492
Simon Warta <simon@warta.it>
2023-10-08 11:27:40 +0300

    Let roundAfterImpl fully operate on bigint

--- drand.ts
+++ drand.ts
@@ -9,15 +9,15 @@ export function timeOfRound(round: number): Date {
   return new Date(DRAND_GENESIS.getTime() + (round - 1) * DRAND_ROUND_LENGTH_MS);
 }
 
-function roundAfterImpl(base: bigint): number {
+function roundAfterImpl(base: bigint): bigint {
   // Losely ported from https://github.com/drand/drand/blob/eb36ba81e3f28c966f95bcd602f60e7ff8ef4c35/chain/time.go#L49-L63
   if (base < DRAND_GENESIS_NS) {
-    return 1;
+    return 1n;
   } else {
     const from_genesis = base - DRAND_GENESIS_NS;
     const periods_since_genesis = from_genesis / DRAND_ROUND_LENGTH_NS;
-    const next_period_index = Number(periods_since_genesis + BigInt(1));
-    return next_period_index + 1; // Convert 0-based counting to 1-based counting
+    const next_period_index = periods_since_genesis + 1n;
+    return next_period_index + 1n; // Convert 0-based counting to 1-based counting
   }
 }
 
@@ -28,5 +28,5 @@ function roundAfterImpl(base: bigint): number {
  * for now this is easier.
  */
 export function roundAfter(after: string | bigint): number {
-  return roundAfterImpl(BigInt(after));
+  return Number(roundAfterImpl(BigInt(after)));
 }
